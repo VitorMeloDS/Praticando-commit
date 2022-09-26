cd68cc29d2003080759fd601c01b9eaa71937420
Vitor Melo da Silva <vitor.melo@fan-edu.com.br>
2022-09-26 15:52:03 -0300

    feat: add function main

--- src/soma.js
+++ src/soma.js
@@ -9,8 +9,11 @@ function multiplicação (a, b) {
   return a * b;
 }
 
-console.log(adicao(85, 964));
-console.log(multiplicação(85, 964));
+const main = () => {
+  console.log(adicao(85, 964));
+  console.log(multiplicação(85, 964));
+  
+  console.log(adicao(23,324));
+  console.log(multiplicação(23,324));
 
-console.log(adicao(23,324));
-console.log(multiplicação(23,324));
+}
