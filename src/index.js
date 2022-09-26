bc474100063cd36a65301c979ec267173fa8ca85
Vitor Melo da Silva <vitor.melo@fan-edu.com.br>
2022-09-26 14:31:29 -0300

    add file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 const fs = require('fs');
 
-function createFile(fileName, file, options) {
+async function createFile(fileName, file, options) {
   const _file = fs.writeFileSync(fileName, file, options);
   console.log(_file);
 };
 
-const valor = {
-  idFilme: 1,
-  nomeFilme: 'XXX',
-  anoLancamento: 'XXXXXX',
-  autor: 'XXXX',
-};
+let valor = '[{"idFilme": 1, "nomeFilme": "XXX", "anoLancamento": "XXXXXX", "autor": "XXXX" }]';
 
-createFile('teste.json', JSON.parse(valor), 'utf8');
+createFile('teste.json', Buffer.from(valor), 'utf8');
